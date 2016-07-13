fbf40d009a07aab6029a2805ccf18b3a7011979c
Daniel Walsh <walsh.daniel@icloud.com>
2016-07-13 14:58:33 -0400

    initial commit

---
+++ Gensler.Learning/Application.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Autodesk.Revit.UI;
+
+namespace Gensler.Learning
+{
+    public class Application : IExternalApplication
+    {
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            application.RegisterDockablePane(new DockablePaneId(Guid.NewGuid()), "Learning Structure", new MainPage());
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ Gensler.Learning/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Autodesk.Revit.UI;
+
+namespace Gensler.Learning
+{
+    /// <summary>
+    /// Interaction logic for MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page, IDockablePaneProvider
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            this.Browser.Navigate("http://10.37.129.2:3000/");
+        }
+
+        public void SetupDockablePane(DockablePaneProviderData data)
+        {
+            data.FrameworkElement = this;
+            //data.InitialState.DockPosition = DockPosition.Tabbed;
+            //data.InitialState.TabBehind = DockablePanes.BuiltInDockablePanes.ProjectBrowser;
+        }
+    }
+}
